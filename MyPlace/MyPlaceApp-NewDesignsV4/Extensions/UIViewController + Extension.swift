ee0c05f2e7ad8e1f897e223cb3d67de5b5cd8c6d
nareshbanavath1839 <88474225+nareshbanavath1839@users.noreply.github.com>
2023-04-20 14:52:37 +0530

    extension Moved

--- MyPlace/MyPlaceApp-NewDesignsV4/Extensions/UIViewController + Extension.swift
+++ MyPlace/MyPlaceApp-NewDesignsV4/Extensions/UIViewController + Extension.swift
@@ -206,6 +206,24 @@ extension UIView {
     
     
 }
+
+extension UIViewController
+{
+    #warning("Storyboard ID must be same as ViewController Name to utilize below method")
+    static func instace(sb : StoryBoard = .newDesignV4) -> Self{
+        
+        let instance = UIStoryboard(name: sb.rawValue, bundle: nil).instantiateViewController(withIdentifier: String(describing: self)) as! Self
+        return instance
+    }
+    
+    enum StoryBoard : String
+    {
+        case newDesignV4 = "NewDesignsV4"
+        case newDesignV5 = "NewDesignsV5"
+        case myPlaceLogin = "MyPlaceLogin"
+    }
+}
+
 extension UINavigationBar {
     
     func shouldRemoveShadow(_ value: Bool) -> Void {
--- MyPlace/MyPlaceApp-NewDesignsV4/TabBarVCs/TabBarVC.swift
+++ MyPlace/MyPlaceApp-NewDesignsV4/TabBarVCs/TabBarVC.swift
@@ -85,23 +85,6 @@ extension TabBarVC : UITabBarControllerDelegate
 }
 
 
-extension UIViewController
-{
-    #warning("Storyboard ID must be same as ViewController Name to utilize below method")
-    static func instace(sb : StoryBoard = .newDesignV4) -> Self{
-        
-        let instance = UIStoryboard(name: sb.rawValue, bundle: nil).instantiateViewController(withIdentifier: String(describing: self)) as! Self
-        return instance
-    }
-    
-    enum StoryBoard : String
-    {
-        case newDesignV4 = "NewDesignsV4"
-        case newDesignV5 = "NewDesignsV5"
-        case myPlaceLogin = "MyPlaceLogin"
-    }
-}
-
 
 struct TabBarItemStruct
 {
